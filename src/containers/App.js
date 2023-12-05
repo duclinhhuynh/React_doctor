169bfe1e51f4c6f1f16609649ed21236fd7b0e4d
linh <linh1862002@gamil.com>
2023-12-05 10:17:47 +0000

    design_header_language_en

--- src/containers/App.js
+++ src/containers/App.js
@@ -17,6 +17,7 @@ import System from '../routes/System';
 
 import { CustomToastCloseButton } from '../components/CustomToast';
 import ConfirmModal from '../components/ConfirmModal';
+import HomePage from'../containers/HomePage/HomePage'
 
 class App extends Component {
 
@@ -51,6 +52,8 @@ class App extends Component {
                                 <Route path={path.HOME} exact component={(Home)} />
                                 <Route path={path.LOGIN} component={userIsNotAuthenticated(Login)} />
                                 <Route path={path.SYSTEM} component={userIsAuthenticated(System)} />
+                                <Route path={path.HOMEPAGE} component={HomePage} />
+
                             </Switch>
                         </span>
 
